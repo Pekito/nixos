aac12be55684b099ee2c8fc11a90a6400d119845
= <p3dr0c4@live.com>
2025-12-03 22:29:05 -0300

    remove x11

--- nixos/shared/default.nix
+++ nixos/shared/default.nix
@@ -63,11 +63,9 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Enable the X11 windowing system
-  services.xserver.enable = true;
-
   # Enable the KDE Plasma Desktop Environment
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
