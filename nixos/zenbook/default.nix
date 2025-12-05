b9e10ab6051fd07c1c5ddc7502dbfdac1ca68860
Pedro Luiz <p3dr0c4@live.com>
2025-12-05 08:36:27 -0300

    add asus-battery to zenbook config

--- nixos/zenbook/default.nix
+++ nixos/zenbook/default.nix
@@ -1,16 +1,23 @@
 # Zenbook laptop configuration
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports = [
     ../shared
     ./hardware-configuration.nix
+    inputs.nixos-hardware.nixosModules.asus-battery
   ];
 
   networking.hostName = "zenbook";
 
   # Zenbook-specific configuration goes here
   # Add any packages or settings specific to this laptop
+
+  # Battery charge limit configuration
+  hardware.asus.battery = {
+    chargeUpto = 80;
+    enableChargeUptoScript = true;
+  };
 
   system.stateVersion = "25.05";
   hardware.bluetooth.enable = true;
