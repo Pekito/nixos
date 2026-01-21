b6aa338491d141be70b2fc28b1d1a8435419fdeb
= <p3dr0c4@live.com>
2026-01-21 09:07:12 -0300

    update swift config

--- nixos/swift/default.nix
+++ nixos/swift/default.nix
@@ -15,6 +15,29 @@
   # Swift-specific configuration goes here
   # Add any packages or settings specific to this laptop
 
+  # Fix audio: Force the correct profile for internal speakers
+  # The Conexant CX11970 audio codec needs the Speaker profile to enable internal speakers
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/51-alsa-swift-audio.conf" ''
+      # Configure audio profile for Swift laptop internal speakers
+      monitor.alsa.rules = [
+        {
+          matches = [
+            {
+              node.name = "~alsa_card.pci-0000_c1_00.6"
+            }
+          ]
+          actions = {
+            update-props = {
+              api.alsa.use-acp = true
+              device.profile = "HiFi (Mic1, Mic2, Speaker)"
+            }
+          }
+        }
+      ]
+    '')
+  ];
+
   system.stateVersion = "25.05";
   hardware.bluetooth.enable = true;
 }
